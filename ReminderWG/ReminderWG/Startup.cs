b1605b4f6233aaa4067c8dd09eb1fbeee20d28d7
jdang9 <dangat3@vcu.edu>
2015-05-07 10:02:55 -0400

    Initial commit

---
+++ ReminderWG/ReminderWG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReminderWG.Startup))]
+namespace ReminderWG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
